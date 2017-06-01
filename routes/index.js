1f7d0a0272169b95be9ea77c75602f5a365beb0e
Ayoub Nejjari <raysoflee07@gmail.com>
2017-06-01 23:55:28 +0000

    finishing login logic

--- routes/index.js
+++ routes/index.js
@@ -52,7 +52,16 @@ router.post('/register', (req, res, next) => {
 });
 // Local strategy
 passport.use(new localStartegy( (username, password, done) => {
-
+  User.findOne({username:username}, (err, user) => {
+    if (err) {return done(err);}
+    if (!user) {
+      return done(null, false, {message: 'Incorrect username.'});
+    }
+    if (!user.validPassword(password)) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+    return done(null, user);
+  });
 }));
 
 // Login processing
